51f5505ec480dcbbfd2e8732bc95f815b3b54fad
Alex Houston <alexpatrickhouston@gmail.com>
2018-01-19 13:37:36 -0800

    First Lab Commit

---
+++ WebApplication4/WebApplication4/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PersonViewer()
+        {
+            ViewBag.Name = "Alex Houston";
+            ViewBag.Address = "123 street";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+
+
+    }
+}
