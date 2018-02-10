5858f273abd03024f734ae5ffbf9046cc2663733
Alex Houston <alexpatrickhouston@gmail.com>
2018-02-10 11:22:24 -0800

    added in the labs i was missing

---
+++ Lab 2/Lab 2/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_2.Data.Entities;
+using Lab_2.Data;
+namespace Lab_2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            user.Id = InMemoryDatabase.NextId();
+            InMemoryDatabase.Users.Add(user);
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            var users = InMemoryDatabase.Users;
+            return View(users);
+        }
+        public ActionResult Details(int id)
+        {
+            User user = InMemoryDatabase.FindUser(id);
+            return View(user);
+        }
+        public ActionResult Delete(int id)
+        {
+            InMemoryDatabase.DeleteUser(id);
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ Lab 2/Lab 2/Data/InMemoryDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab_2.Data.Entities;
+
+namespace Lab_2.Data
+{
+    public static class InMemoryDatabase
+    {
+        public static List<User> Users = new List<User>();
+        public static int id = 0;
+        public static int NextId()
+        {
+            return id++;
+        }
+        public static void DeleteUser(int id)
+        {
+            var user = Users.Find(u => u.Id == id);
+            Users.Remove(user);
+        }
+        public static User FindUser(int id)
+        {
+            var user = Users.Find(u => u.Id == id);
+            return user;
+        }
+    }
+}
