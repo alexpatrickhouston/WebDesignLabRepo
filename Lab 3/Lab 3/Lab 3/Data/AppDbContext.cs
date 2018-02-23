e2960377bdef300272fe0aa44ebe2bec3ccb2de8
Alex Houston <alexpatrickhouston@gmail.com>
2018-02-22 16:15:53 -0800

    lab 6

---
+++ Lab 3/Lab 3/Lab 3/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Lab_3.Data.Entities;
+
+namespace Lab_3.Models
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ lab6/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿function display() {
+    alert("displaying");
+    var firstname = document.getElementById("FName").value;
+    var lastname = document.getElementById("LName").value;
+    var age = document.getElementById("age").value;
+    var summary = firstname + " " + lastname + " is " + age + " years old.";
+    document.getElementById("summary").innerText = summary;
+}
+function validate() {
+    if (document.getElementById("FName").value == "")
+        alert("First Name must be filled out");
+    return false;
+    if (document.getElementById("LName").value == "")
+        alert("Last Name must be filled out");
+    return false;
+    if (document.getElementById("age").value == "")
+        alert("age must be filled out");
+    return false;
+}
